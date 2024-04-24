e83e12e0a36febadba30ae7320b77fe8d98b3bd8
AngelAmoFP <angamosan@alum.us.es>
2024-04-24 15:31:35 +0200

    Backend COMPLETO

--- DeliverUS-Backend/src/routes/OrderRoutes.js
+++ DeliverUS-Backend/src/routes/OrderRoutes.js
@@ -7,23 +7,24 @@ import { Order } from '../models/models.js'
 import { handleValidation } from '../middlewares/ValidationHandlingMiddleware.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for:
+  // DONE: Include routes for:
   // 1. Retrieving orders from current logged-in customer
   app.route('/orders')
     .get(
-      isLoggedIn, // we check it's logged-in
-      hasRole('customer'), // we check it has role customer
-      OrderController.indexCustomer // function that queries orders from current logged-in customer and send them back
+      isLoggedIn,
+      hasRole('customer'),
+      OrderController.indexCustomer
     )
+
   // 2. Creating a new order (only customers can create new orders)
   app.route('/orders')
     .post(
-      isLoggedIn, // we check it's logged-in
-      hasRole('customer'), // we check it has role customer
-      OrderMiddleware.checkRestaurantExists, // we check if the restaurant of the order exists
-      OrderValidation.create, // we validate data to create the order
-      handleValidation, // we send errors
-      OrderController.create // we create if everything goes well
+      isLoggedIn,
+      hasRole('customer'),
+      OrderMiddleware.checkRestaurantExists,
+      OrderValidation.create,
+      handleValidation,
+      OrderController.create
     )
   app.route('/orders/:orderId/confirm')
     .patch(
@@ -34,7 +35,6 @@ const loadFileRoutes = function (app) {
       OrderMiddleware.checkOrderIsPending,
       OrderController.confirm
     )
-
   app.route('/orders/:orderId/send')
     .patch(
       isLoggedIn,
@@ -53,27 +53,28 @@ const loadFileRoutes = function (app) {
       OrderMiddleware.checkOrderCanBeDelivered,
       OrderController.deliver)
 
-  // TODO: Include routes for:
+  // DONE: Include routes for:
   // 3. Editing order (only customers can edit their own orders)
   app.route('/orders/:orderId')
     .put(
-      isLoggedIn, // we check it's logged-in
-      hasRole('customer'), // we check it has role customer
-      checkEntityExists(Order, 'orderId'), // we check that order with that ID exists
-      OrderMiddleware.checkOrderCustomer, // we check if the order belongs to current loggedIn customer
-      OrderMiddleware.checkOrderIsPending, // we check the order is still pending
-      OrderValidation.update, // we validate data to update the order
-      handleValidation, // we send back errors
-      OrderController.update) // we update the order if everything goes well
+      isLoggedIn,
+      hasRole('customer'),
+      checkEntityExists(Order, 'orderId'),
+      OrderMiddleware.checkOrderCustomer,
+      OrderMiddleware.checkOrderIsPending,
+      OrderValidation.update,
+      handleValidation,
+      OrderController.update)
+
   // 4. Remove order (only customers can remove their own orders)
   app.route('/orders/:orderId')
     .delete(
-      isLoggedIn, // we check if it's logged-in
-      hasRole('customer'), // we check it has role customer
-      checkEntityExists(Order, 'orderId'), // we check that order with that ID exists
-      OrderMiddleware.checkOrderCustomer, // we check if the order belongs to current loggedIn customer
-      OrderMiddleware.checkOrderIsPending, // we check the order is still pending
-      handleValidation, // we send back errors
+      isLoggedIn,
+      hasRole('customer'),
+      checkEntityExists(Order, 'orderId'),
+      OrderMiddleware.checkOrderCustomer,
+      OrderMiddleware.checkOrderIsPending,
+      handleValidation,
       OrderController.destroy)
 
   app.route('/orders/:orderId')
